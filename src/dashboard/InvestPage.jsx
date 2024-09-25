11325d1b0891acf6ae9646ed8c12522db92cca05
Yhucee14 <uchechukwu7814@gmail.com>
2024-09-25 09:43:23 -0700

    inventory page

--- src/dashboard/InvestPage.jsx
+++ src/dashboard/InvestPage.jsx
@@ -6,6 +6,12 @@ import { Link } from "react-router-dom";
 import GreenFooter from "./GreenFooter";
 import eye from "../assets/eye.png";
 import search from "../assets/search.png";
+import add from "../assets/add.png";
+import tomato from "../assets/tomato.png";
+import gridone from "../assets/gridone.png";
+import gridtwo from "../assets/gridtwo.png";
+import gridthree from "../assets/gridthree.png";
+import gridfour from "../assets/gridfour.png";
 
 const Invest = () => {
   const [assets, setAssets] = useState([]);
@@ -100,7 +106,7 @@ const Invest = () => {
 
           {/*section two*/}
           <div className="flex py-5 px-2 mt-3 border flex-row justify-between">
-            <div className="flex flex-row justify-between gap-2">
+            <div className="flex flex-row justify-between">
               <button
                 className={`border-b-4 flex justify-center items-center font-semibold text-lg px-14 ${
                   activeSection === "Invest"
@@ -220,6 +226,174 @@ const Invest = () => {
             <div className="py-5 px-2 flex flex-col">
               <div className="font-bold py-2 text-2xl px-2">
                 NFT Inventory Marketplace
+              </div>
+
+              <div className="flex py-5 px-1 mt-3 flex-row justify-between gap-4">
+                <div className="flex flex-row justify-between gap-2">
+                  <button className="border-b-4 flex justify-center items-center font-semibold text-md px-5">
+                    Most viewed
+                  </button>
+
+                  <button
+                    className={`border-b-4  flex justify-center items-center font-semibold text-md px-5`}
+                  >
+                    Unsold
+                  </button>
+
+                  <button
+                    className={`border-b-4 flex justify-center items-center font-semibold text-md px-5`}
+                  >
+                    Sold
+                  </button>
+                </div>
+
+                <div className="flex flex-row gap-2">
+                  <div className="relative flex xx:w-24 md:w-[400px]   shadow-sm">
+                    <div className="w-full py-3 border px-14 rounded-md">
+                      <input
+                        type="text"
+                        placeholder="Search Collection"
+                        className=" focus:outline-none focus:ring-[#359A35] focus:border-[#A4A4A4]"
+                      />
+                    </div>
+
+                    <div className="absolute inset-y-0 left-0 flex px-3 cursor-pointer items-center">
+                      <img
+                        src={search}
+                        alt="Search Icon"
+                        className="h-[25px] w-[25px] bg-transparent"
+                        style={{ fill: "white" }}
+                      />
+                    </div>
+                  </div>
+
+                  <div className=" flex flex-row">
+                    <button className="flex flex-row text-white rounded-md hover:bg-white hover:text-[#359A35] hover:border-2 hover:border-[#359A35] transition-all duration-300 bg-[#359A35] py-2 px-3 justify-center items-center">
+                      <div>
+                        <img src={add} alt="home" className=" " />
+                      </div>
+                      <div className="font-medium px-1 ">Connect Wallet</div>
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              {/* Table structure */}
+              <div className="py-4 flex justify-center">
+                <table className=" table-auto border-collapse border border-gray-300">
+                  <thead>
+                    <tr className="bg-[#f1f1f1]">
+                      <th className="px-4 py-2 border border-gray-300 flex justify-start">
+                        # Collections
+                      </th>
+                      <th className="px-2 py-2 border border-gray-300 text-center">
+                        Floor Price
+                      </th>
+                      <th className="px-2 py-2 border border-gray-300 text-center">
+                        Changes
+                      </th>
+                      <th className="px-2 py-2 border border-gray-300 text-center">
+                        Market Cap
+                      </th>
+                      <th className="px-2 py-2 border border-gray-300 text-center">
+                        Listed Assets
+                      </th>
+                      <th className="px-2 py-2 border border-gray-300 text-center">
+                        Owners
+                      </th>
+                      <th className="px-2 py-2 border border-gray-300 text-center">
+                        Sales per day
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className="hover:bg-gray-100">
+                      <td className="px-2 py-2 border border-gray-300">
+                        <div className="flex flex-row py-2 gap-2">
+                          <p className="font-semibold flex justify-center items-center px-2">
+                            1
+                          </p>
+                          <div className="flex flex-row gap-2">
+                            <div className="rounded-full">
+                              <img
+                                src={tomato}
+                                alt="home"
+                                className="w-10 h-10 "
+                              />
+                            </div>
+
+                            <div className="flex flex-col">
+                              <p className="font-bold">BIMF</p>
+                              <p>From: Bimbo maize farm</p>
+                            </div>
+                          </div>
+                        </div>
+                      </td>
+
+                      <td className="px-2 py-2 gap-2 text-center flex flex-row items-center border-gray-300">
+                        <div className="font-bold flex justify-center text-center">
+                          <p className="mt-[-14px]">33.02K</p>
+                        </div>
+                        <div className="flex flex-col">
+                          <p className="mt-3 text-sm">USDT</p>
+                          <p className="text-sm">($3.07)</p>
+                        </div>
+                      </td>
+
+                      <td className="px-2 py-2 text-[#359A35] border font-bold text-center border-gray-300">
+                        <p>4.35%</p>
+                      </td>
+
+                      <td className="px-2 py-2 border font-bold border-gray-300">
+                        $90.270 USDT
+                      </td>
+
+                      <td className="px-2 py-2 text-[#2210F2]  text-center items-center font-semibold border-gray-300">
+                        100
+                      </td>
+
+                      <td className="px-2 py-2 border text-center font-semibold border-gray-300">
+                        307
+                      </td>
+
+                      <td className="px-2 py-2 border text-center font-semibold border-gray-300">
+                        20
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+
+              <div className="py-4 px-2">
+                <div className="text-gradient-invest font-bold text-lg">
+                  <p>Own Your Digital Assets</p>
+                </div>
+
+                <div className="w-[600px] text-[#736D6D] font-medium py-5">
+                  <p>
+                    Start your own NFT storefront in minutes with Cropcrypt.
+                    Mint, sell, and scale your NFTs all in one customizable
+                    frontend with low fees.
+                  </p>
+                </div>
+
+                <div className="grid grid-cols-4 py-5 gap-2">
+                  <div className=" rounded-md">
+                    <img src={gridfour} alt="home" className=" " />
+                  </div>
+
+                  <div className=" rounded-md">
+                    <img src={gridthree} alt="home" className=" " />
+                  </div>
+
+                  <div className=" rounded-md">
+                    <img src={gridtwo} alt="home" className=" " />
+                  </div>
+
+                  <div className=" rounded-md">
+                    <img src={gridone} alt="home" className="h-[230px] " />
+                  </div>
+                </div>
               </div>
             </div>
           )}
