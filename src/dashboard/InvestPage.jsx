82f79ba1904378bb1532bf1f93b034f160bd5e57
Yhucee14 <uchechukwu7814@gmail.com>
2024-11-30 08:54:38 -0800

    progress

--- src/dashboard/InvestPage.jsx
+++ src/dashboard/InvestPage.jsx
@@ -24,9 +24,7 @@ const Invest = () => {
   useEffect(() => {
     const fetchAssets = async () => {
       try {
-        const response = await axios.get(
-          `${import.meta.env.VITE_ASSET}`
-        );
+        const response = await axios.get(`${import.meta.env.VITE_ASSET}`);
         setAssets(response.data.data || []);
       } catch (error) {
         console.error(
@@ -52,7 +50,7 @@ const Invest = () => {
       {isLoading ? (
         <div className="flex justify-center items-center h-64">
           <div className="w-14 h-14 border-4 border-t-4 border-gray-200 rounded-full animate-spin border-t-green-500"></div>
-        </div> 
+        </div>
       ) : (
         <div className="xx:px-2 sm:px-6">
           {/*section one*/}
@@ -98,8 +96,8 @@ const Invest = () => {
                   </div>
 
                   <div className="md:mt-2 py-1 md:py-0">
-                  <p className="px-2 md:px-2 py-1 text-xs md:text-sm bg-[#D8ECD8] rounded-xl text-[#075607] font-bold">
-                    24.7%
+                    <p className="px-2 md:px-2 py-1 text-xs md:text-sm bg-[#D8ECD8] rounded-xl text-[#075607] font-bold">
+                      24.7%
                     </p>
                   </div>
                 </div>
@@ -108,40 +106,40 @@ const Invest = () => {
           </div>
 
           <div className="flex ss:hidden py-4 px-4 -mt-8 xx:flex-row justify-between sm:flex-row  gap-2 md:gap-3">
-              <div className="flex flex-col shadow-lg xx:gap-4 md:gap-0   md:w-full bg-white p-2 md:p-3 rounded-md">
-                <p className="text-black font-bold py-1 md:text-xl">
-                  Total Invest
-                </p>
-                <div className="flex flex-row sm:justify-center gap-2 md:gap-4">
-                  <div className="text-[#359A35] font-extrabold text-lg md:text-4xl">
-                    <p className="">$600.00</p>
-                  </div>
-
-                  <div className="md:mt-2 py-1">
-                    <p className="px-2 md:px-2 py-1 text-xs md:text-sm bg-[#F5B30A] rounded-xl text-black font-bold">
-                      50 stock
-                    </p>
-                  </div>
+            <div className="flex flex-col shadow-lg xx:gap-4 md:gap-0   md:w-full bg-white p-2 md:p-3 rounded-md">
+              <p className="text-black font-bold py-1 md:text-xl">
+                Total Invest
+              </p>
+              <div className="flex flex-row sm:justify-center gap-2 md:gap-4">
+                <div className="text-[#359A35] font-extrabold text-lg md:text-4xl">
+                  <p className="">$600.00</p>
                 </div>
-              </div>
 
-              <div className="flex flex-col shadow-lg xx:gap-4 md:gap-0   md:w-full bg-white p-2 md:p-3 rounded-md">
-                <p className="text-black font-bold py-1 md:text-xl">
-                Total Return
-                </p>
-                <div className="flex flex-row justify-between sm:justify-center gap-2 md:gap-4">
-                  <div className="text-[#359A35] font-extrabold text-lg md:text-4xl">
-                    <p className="">$300.00</p>
-                  </div>
-
-                  <div className="md:mt-2 py-1">
-                    <p className="px-2 md:px-2 py-1 text-xs md:text-sm bg-[#D8ECD8] rounded-xl text-[#075607] font-bold">
-                    24.7%
-                    </p>
-                  </div>
+                <div className="md:mt-2 py-1">
+                  <p className="px-2 md:px-2 py-1 text-xs md:text-sm bg-[#F5B30A] rounded-xl text-black font-bold">
+                    50 stock
+                  </p>
                 </div>
               </div>
             </div>
+
+            <div className="flex flex-col shadow-lg xx:gap-4 md:gap-0   md:w-full bg-white p-2 md:p-3 rounded-md">
+              <p className="text-black font-bold py-1 md:text-xl">
+                Total Return
+              </p>
+              <div className="flex flex-row justify-between sm:justify-center gap-2 md:gap-4">
+                <div className="text-[#359A35] font-extrabold text-lg md:text-4xl">
+                  <p className="">$300.00</p>
+                </div>
+
+                <div className="md:mt-2 py-1">
+                  <p className="px-2 md:px-2 py-1 text-xs md:text-sm bg-[#D8ECD8] rounded-xl text-[#075607] font-bold">
+                    24.7%
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
 
           {/*section two*/}
           <div className="flex py-5 px-2 sm:px-2 mt-3 border xx:flex-col sm:flex-row xx:gap-4 sm:gap-0 justify-between">
@@ -213,49 +211,61 @@ const Invest = () => {
                           className="z-20 md:h-[330px] h-[174px] w-full"
                         />
                       </div>
+
+                      <div className="flex flex-col">
                       <div className="flex sm:py-5 flex-row justify-between md:gap-2 lg:gap-8">
-                        <div className="flex flex-col px-4 py-2">
+                        <div className="flex flex-col px-2 py-2">
                           <p className="font-bold py-2 md:text-3xl">
                             {asset.name}
                           </p>
                           <div className="text-[#359A35] flex flex-row gap-2 font-extrabold md:text-2xl">
                             <p>{asset.amount / asset.shares}</p>
                             <p className="text-[#A8A4A4] flex items-center md:text-xl">
-                              USDT
+                              RWA
                             </p>
                           </div>
                           <p className="text-[#A8A4A4] font-bold md:text-lg">
                             Price per 1 share
                           </p>
-                          <div className="mt-9">
-                            <p className="py-3 text-md">
-                              Annual Return: {asset.roi || "N/A"}%
-                            </p>
-                            <p className="text-md">
-                              Annual Appreciation:{" "}
-                              {asset.roi || "N/A"}%
-                            </p>
-                          </div>
                         </div>
-                        <div className="flex flex-col justify-between sm:gap-20 px-1 py-5">
-                          <div className="flex flex-row gap-2">
-                            <div>
-                              <img src={share} alt="share" className="z-20 w-9 h-9 md:w-14 md:h-14" />
-                            </div>
-                            <button className="bg-[#359A35] rounded-xl md:py-2 px-3 md:px-6 font-bold text-white">
+
+                        <div className="flex py-4 px-2 flex-row gap-2">
+                          <div>
+                            <img
+                              src={share}
+                              alt="share"
+                              className="z-20 w-10 h-10 md:w-14 md:h-14"
+                            />
+                          </div>
+                          <div>
+                            <button className="bg-[#359A35] rounded-xl py-2 md:py-2 px-3 md:px-6 font-bold text-white">
                               BUY
                             </button>
                           </div>
-                          <div className="flex flex-col">
-                            <p className="py-3 text-md">
-                              Share Price: ${asset.amount || "N/A"}
-                            </p>
-                            <p className="text-md">
-                              Available shares: {asset.shares}
-                            </p>
-                          </div>
                         </div>
                       </div>
+
+                      <div className="flex flex-row justify-between sm:gap-20 px-2 py-5">
+                        <div className="">
+                          <p className="py-3 text-md">
+                            Annual Return: {asset.roi || "N/A"}%
+                          </p>
+                          <p className="text-md">
+                            Annual Appreciation: {asset.roi || "N/A"}%
+                          </p>
+                        </div>
+
+                        <div className="flex flex-col">
+                          <p className="py-3 text-md">
+                            Share Price: ${asset.amount || "N/A"}
+                          </p>
+                          <p className="text-md">
+                            Available shares: {asset.shares}
+                          </p>
+                        </div>
+                      </div>
+                      </div>
+                   
                     </div>
                   </div>
                 </Link>
@@ -347,60 +357,62 @@ const Invest = () => {
                   </thead>
 
                   <tbody className="">
-                      <tr className="hover:bg-[#359A351A] cursor-pointer"
-                     onClick={() => navigate('/nft')} >
-                        <td className="px-2 py-2 border border-gray-300">
-                          <div className="flex flex-row py-2 gap-2">
-                            <p className="font-semibold flex justify-center items-center px-2">
-                              1
-                            </p>
-                            <div className="flex flex-row gap-2">
-                              <div className="rounded-full">
-                                <img
-                                  src={tomato}
-                                  alt="home"
-                                  className="w-10 h-10 "
-                                />
-                              </div>
+                    <tr
+                      className="hover:bg-[#359A351A] cursor-pointer"
+                      onClick={() => navigate("/nft")}
+                    >
+                      <td className="px-2 py-2 border border-gray-300">
+                        <div className="flex flex-row py-2 gap-2">
+                          <p className="font-semibold flex justify-center items-center px-2">
+                            1
+                          </p>
+                          <div className="flex flex-row gap-2">
+                            <div className="rounded-full">
+                              <img
+                                src={tomato}
+                                alt="home"
+                                className="w-10 h-10 "
+                              />
+                            </div>
 
-                              <div className="flex flex-col">
-                                <p className="font-bold">BIMF</p>
-                                <p>From: Bimbo maize farm</p>
-                              </div>
+                            <div className="flex flex-col">
+                              <p className="font-bold">BIMF</p>
+                              <p>From: Bimbo maize farm</p>
                             </div>
                           </div>
-                        </td>
+                        </div>
+                      </td>
 
-                        <td className="px-2 py-2 gap-2 text-center flex flex-row items-center border-gray-300">
-                          <div className="font-bold flex justify-center text-center">
-                            <p className="mt-[-14px]">33.02K</p>
-                          </div>
-                          <div className="flex flex-col">
-                            <p className="mt-3 text-sm">USDT</p>
-                            <p className="text-sm">($3.07)</p>
-                          </div>
-                        </td>
+                      <td className="px-2 py-2 gap-2 text-center flex flex-row items-center border-gray-300">
+                        <div className="font-bold flex justify-center text-center">
+                          <p className="mt-[-14px]">33.02K</p>
+                        </div>
+                        <div className="flex flex-col">
+                          <p className="mt-3 text-sm">USDT</p>
+                          <p className="text-sm">($3.07)</p>
+                        </div>
+                      </td>
 
-                        <td className="px-2 py-2 text-[#359A35] border font-bold text-center border-gray-300">
-                          <p>4.35%</p>
-                        </td>
+                      <td className="px-2 py-2 text-[#359A35] border font-bold text-center border-gray-300">
+                        <p>4.35%</p>
+                      </td>
 
-                        <td className="px-2 py-2 border font-bold border-gray-300">
-                          $90.270 USDT
-                        </td>
+                      <td className="px-2 py-2 border font-bold border-gray-300">
+                        $90.270 USDT
+                      </td>
 
-                        <td className="px-2 py-2 text-[#2210F2]  text-center items-center font-semibold border-gray-300">
-                          100
-                        </td>
+                      <td className="px-2 py-2 text-[#2210F2]  text-center items-center font-semibold border-gray-300">
+                        100
+                      </td>
 
-                        <td className="px-2 py-2 border text-center font-semibold border-gray-300">
-                          307
-                        </td>
+                      <td className="px-2 py-2 border text-center font-semibold border-gray-300">
+                        307
+                      </td>
 
-                        <td className="px-2 py-2 border text-center font-semibold border-gray-300">
-                          20
-                        </td>
-                      </tr>
+                      <td className="px-2 py-2 border text-center font-semibold border-gray-300">
+                        20
+                      </td>
+                    </tr>
                   </tbody>
                 </table>
               </div>
@@ -432,7 +444,11 @@ const Invest = () => {
                   </div>
 
                   <div className="border rounded-md">
-                    <img src={gridone} alt="home" className="xx:w-full md:h-[230px] " />
+                    <img
+                      src={gridone}
+                      alt="home"
+                      className="xx:w-full md:h-[230px] "
+                    />
                   </div>
                 </div>
               </div>
