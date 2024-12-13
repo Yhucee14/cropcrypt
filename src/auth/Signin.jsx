d08fbcefd61d5cb82a427cb031b31e6863de865b
Yhucee14 <uchechukwu7814@gmail.com>
2024-12-12 22:20:57 -0800

    waiting for endpoint

--- src/auth/Signin.jsx
+++ src/auth/Signin.jsx
@@ -1,27 +1,53 @@
 import { useState } from "react";
-import AuthNav from "./AuthNav"
+import AuthNav from "./AuthNav";
 import google from "../assets/google.png";
 import apple from "../assets/apple.png";
 import { Link } from "react-router-dom";
+import useSignInMutation from "../mutations/useSignInMutation";
 
 const Signin = () => {
-    const [passwordVisible, setPasswordVisible] = useState(false);
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [errorMessage, setErrorMessage] = useState("");
+  const signInMutation = useSignInMutation;
 
-    const togglePasswordVisibility = () => {
-      setPasswordVisible(!passwordVisible);
-    };
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setErrorMessage(""); // Clear any previous error messages
+
+    signInMutation.mutate(formData, {
+      onError: (error) => {
+        const message =
+          error.response?.data?.message || "An unexpected error occurred.";
+        setErrorMessage(message); // Display error in the UI
+      },
+      onSuccess: (data) => {
+        console.log("Sign-in successful:", data);
+        // Navigate to a different page or save the token to localStorage
+      },
+    });
+  };
 
   return (
     <div className="overflow-x-hidden">
-        <AuthNav />
+      <AuthNav />
 
-        <div className="flex flex-col ss:w-[500px] mx-auto pt-2 ss:pt-0 ss:justify-center">
+      <div className="flex flex-col ss:w-[500px] mx-auto pt-2 ss:pt-0 ss:justify-center">
         <div className="flex flex-col  justify-center ">
           <span className="font-bold flex justify-center text-2xl ss:text-3xl py-2">
-          Sign in to Cropcrypt
+            Sign in to Cropcrypt
           </span>
           <p className="font-normal flex justify-center py-2 text-xs ss:text-sm ss:text-center">
-          Please fill in your details to gain secured access to your account
+            Please fill in your details to gain secured access to your account
           </p>
         </div>
 
@@ -51,78 +77,107 @@ const Signin = () => {
           <span className="border w-20 ss:w-40 h-0"></span>
         </div>
 
-        <div>
-          <form className="flex flex-col justify-between gap-4  w-full">
-            <div className="relative w-full xx:px-4 sm:px-0 flex flex-col gap-8 mx-auto">
-
-              <div>
-                <input
-                  type="text"
-                  placeholder="Email address"
-                  className="border border-gray-400 transition-all duration-300 bg-[#F3F3F3] focus:bg-[#E0EEE0] focus:border-2 focus:border-[#359A35] w-full rounded-xl h-12 text-sm pl-4 text-[#1A0F28] focus:outline-none"
-                />
-              </div>
-
-              <div className="relative">
-                <input
-                  type={passwordVisible ? "text" : "password"}
-                  placeholder="Password"
-                  className="border border-gray-400 transition-all duration-300 bg-[#F3F3F3] focus:bg-[#E0EEE0] focus:border-2 focus:border-[#359A35] w-full rounded-xl h-12 text-sm pl-4 text-[#1A0F28] focus:outline-none"
-                />
-                <button
-                  type="button"
-                  className="absolute right-4 top-1/2 transform -translate-y-1/2 text-xs text-gray-600"
-                  onClick={togglePasswordVisibility}
-                >
-                  {passwordVisible ? "Hide" : "Show"}
-                </button>
-              </div>
-
-              <div className="flex px-1 ss:px-0 ss:py-0 ss:justify-center font-medium text-xs">
-                Forgot Password ? <Link to='forgotPassword' className="px-1 cursor-pointer text-[#3d80de] underline"> Click here.</Link>
-              </div>
-
-              <div className="flex flex-col justify-center w-full">
-                <button
-                  type="submit"
-                  className="flex w-full gap-2 h-12 transition-all duration-300 bg-[#359A35] hover:bg-[white] hover:border-2 hover:border-gray-400 hover:text-[#359A35] rounded-xl py-2 px-4 text-white justify-center items-center"
-                >
-                  <div>Sign in</div>
-                </button>
-              </div>
-            </div>
-            
-            <Link to='/verifyemail'>Verify</Link>
-
-            <div className="px-5">
-              <p className="text-[#1A0F28] text-xs text-center">
-                By signing in, you agree to Cropcrypt’s
-                <a
-                  href="URL"
-                  target="target"
-                  className="text-[#3d80de] underline text-xs"
-                >
-                  {" "}
-                  Terms of Service and Privacy Policy.
-                </a>
-              </p>
-
-              <p className="text-center text-xs py-5 ss:py-3">
-                Have an account?{" "}
-                <Link to="/signin" className="text-[#359A35] underline cursor-pointer text-xs ">
-                  Sign In
-                </Link>
-              </p>
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col justify-between gap-4  w-full"
+        >
+          <div className="relative w-full xx:px-4 sm:px-0 flex flex-col gap-8 mx-auto">
+            <div>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                placeholder="Email"
+                required
+                className="border border-gray-400 transition-all duration-300 bg-[#F3F3F3] focus:bg-[#E0EEE0] focus:border-2 focus:border-[#359A35] w-full rounded-xl h-12 text-sm pl-4 text-[#1A0F28] focus:outline-none"
+              />
             </div>
 
-            <div className="text-center  text-[#00000099] text-base">
-              @2024-2025 Cropcrypt. All rights reserved.
+            <div className="relative">
+              <input
+                type={passwordVisible ? "text" : "password"}
+                name="password"
+                value={formData.password}
+                onChange={handleInputChange}
+                placeholder="Password"
+                required
+                className="border border-gray-400 transition-all duration-300 bg-[#F3F3F3] focus:bg-[#E0EEE0] focus:border-2 focus:border-[#359A35] w-full rounded-xl h-12 text-sm pl-4 text-[#1A0F28] focus:outline-none"
+              />
+              <button
+                type="button"
+                className="absolute right-4 top-1/2 transform -translate-y-1/2 text-xs text-gray-600"
+                onClick={togglePasswordVisibility}
+              >
+                {passwordVisible ? "Hide" : "Show"}
+              </button>
             </div>
-          </form>
-        </div>
+
+            <div className="flex px-1 ss:px-0 ss:py-0 ss:justify-center font-medium text-xs">
+              Forgot Password ?{" "}
+              <Link
+                to="forgotPassword"
+                className="px-1 cursor-pointer text-[#3d80de] underline"
+              >
+                {" "}
+                Click here.
+              </Link>
+            </div>
+
+            {errorMessage && (
+              <div className="error-message">{errorMessage}</div>
+            )}
+
+            <div className="flex flex-col justify-center w-full">
+              <button
+                type="submit"
+                disabled={signInMutation.isLoading}
+                className="flex w-full gap-2 h-12 transition-all duration-300 bg-[#359A35] hover:bg-[white] hover:border-2 hover:border-gray-400 hover:text-[#359A35] rounded-xl py-2 px-4 text-white justify-center items-center"
+              >
+                {signInMutation.isLoading ? (
+                  <div className="flex justify-center items-center h-64">
+                    <div className="w-14 h-14 border-4 border-t-4 border-gray-200 rounded-full animate-spin border-t-green-500"></div>
+                  </div>
+                ) : (
+                  "Sign In"
+                )}
+              </button>
+            </div>
+          </div>
+
+          <Link to="/verifyemail">Verify</Link>
+
+          <div className="px-5">
+            <p className="text-[#1A0F28] text-xs text-center">
+              By signing in, you agree to Cropcrypt’s
+              <a
+                href="URL"
+                target="target"
+                className="text-[#3d80de] underline text-xs"
+              >
+                {" "}
+                Terms of Service and Privacy Policy.
+              </a>
+            </p>
+
+            <p className="text-center text-xs py-5 ss:py-3">
+              Have an account?{" "}
+              <Link
+                to="/signin"
+                className="text-[#359A35] underline cursor-pointer text-xs "
+              >
+                Sign In
+              </Link>
+            </p>
+          </div>
+
+          <div className="text-center  text-[#00000099] text-base">
+            @2024-2025 Cropcrypt. All rights reserved.
+          </div>
+        </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signin
+export default Signin;
